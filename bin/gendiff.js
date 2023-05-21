81d068a2cb54e3840eda8b66234dcca2aba7c1ff
Maxim Samorukov <maxim.samorukov@gmail.com>
2023-05-21 15:59:42 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,12 @@ const { name, version, description} = require('../package.json');
 program
    .name(name)
    .version(version)
-   .description(description);
-program.parse();
+   .description(description)
+   .option('-f, --format <type>', 'output format')
+   .helpOption('-h, --help', 'output usage information')
+   .argument('<filepath1> <filepath2>');
 
+program.parse(process.argv);
+
+const options = program.args;
+console.log(options)
